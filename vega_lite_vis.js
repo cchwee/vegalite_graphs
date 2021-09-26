a785e41cda44897f0be377224fc6c7fe7f45c2a5
Cassandra Chwee <cc.cassandrachwee@gmail.com>
2021-09-26 18:24:55 +0800

    changes

---
+++ vega_lite_vis.js
@@ -0,0 +1,7 @@
+var vg_1 = "map.json";
+
+vegaEmbed("#map_idiom", vg_1).then(function(result) {
+
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
