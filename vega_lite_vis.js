617b248185771e406a60b019947db14038948b95
Cassandra Chwee <cc.cassandrachwee@gmail.com>
2021-10-10 18:20:53 +0800

    changes to index file

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -5,3 +5,11 @@ vegaEmbed("#map_idiom", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 
 }).catch(console.error);
+
+var vg_2 = "scatterplot.json";
+
+vegaEmbed("#scatterplot_idiom", vg_2).then(function(result) {
+
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
